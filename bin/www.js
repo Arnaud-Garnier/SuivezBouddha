91097bc47aa1411cc5efe454cec59ed3f49185ad
Donzelle <theo.donzelle@gmail.com>
2017-01-06 08:34:12 +0100

    Creation methode read et writeFile et message On setRoom

--- bin/www.js
+++ bin/www.js
@@ -23,6 +23,41 @@ app.set('port', port);
 var server = http.createServer(app);
 
 /**
+* Write file
+*/
+function writeFile (path, value) {
+    var promise = new Promise(function(resolve, reject) {
+        fs.writeFile(path, value, function(err) {
+            if(err) {
+                reject(err);
+            } else {
+                console.log("File " + path + " saved");
+                resolve();
+            }
+        }); 
+    });
+    return promise;
+}
+
+/**
+* Read file
+*/
+function readFile (path) {
+    var promise = new Promise(function(resolve, reject) {
+        fs.readFile(path, 'utf8', function (err, data) {
+            if(err) {
+                reject();
+            } else {
+                console.log("File " + path + " red");
+                resolve(data);
+            }
+        }
+        )
+    })
+    return promise;
+}
+
+/**
 * Create Socket.IO
 */
 
@@ -30,6 +65,7 @@ var io = require('socket.io').listen(server);
 
 // Quand un client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket) {
+    console.log("------------------");
     console.log('New Client');
 
     // Quand un message arrive, on le note dans la console
@@ -38,14 +74,16 @@ io.sockets.on('connection', function (socket) {
     });
 
     socket.on('askDirection', function (directionId) {
+        console.log("------------------");
         console.log("Direction asked (id : " + directionId + ")");
         fs.readFile('ressources/directions.json', 'utf8', function (err, data) {
             if (err) throw err;
+            console.log("File ressources/directions.json open");
             var directions = JSON.parse(data).directions;
             for(var i = 0; i < directions.length; i++){
                 if(directions[i].id == directionId){
-                    console.log("Position found (id : " + directionId + ")");
-                    console.log("Emit : " + directions[i]);
+                    console.log("Direction found (id : " + directionId + ")");
+                    console.log("Emit : " + JSON.stringify(directions[i]));
                     socket.emit('newDirection', directions[i]);
                     break;
                 }
@@ -54,30 +92,58 @@ io.sockets.on('connection', function (socket) {
     });
 
     socket.on('askPosition', function (positionId) {
+        console.log("------------------");
         console.log("Position asked (id : " + positionId + ")");
-        fs.readFile('ressources/positions.json', 'utf8', function (err, data) {
-            if (err) throw err;
-            var positions = JSON.parse(data).positions;
-            for(var i = 0; i < positions.length; i++){
-                if(positions[i].id == positionId){
-                    console.log("Position found (id : " + positionId + ")");
-                    console.log("Emit : " + positions[i]);
-                    socket.emit('newPosition', positions[i]);
-                    break;
+        readFile('ressources/positions.json').then(
+            function(data) {
+                var positions = JSON.parse(data).positions;
+                for(var i = 0; i < positions.length; i++){
+                    if(positions[i].id == positionId){
+                        console.log("Position found (id : " + positionId + ")");
+                        console.log("Emit : " + JSON.stringify(positions[i]));
+                        socket.emit('newPosition', positions[i]);
+                        break;
+                    }
                 }
-            }
-        });
+            })
     });
 
+
     socket.on('askAllRooms', function (positionId) {
+        console.log("------------------");
         console.log('All rooms asked');
-        fs.readFile('ressources/rooms.json', 'utf8', function (err, data) {
-            if (err) throw err;
-            console.log("File ressources/rooms.json open");
-            console.log("Emit : " + data);
-            socket.emit('allRooms', data);
-        });
+        readFile('ressources/rooms.json').then(
+            function(data) {
+                console.log("Emit : " + JSON.stringify(data));
+                socket.emit('allRooms', data);
+            })
     });
+
+    socket.on('setRoom', function(room){
+        console.log("------------------");
+        console.log("Setting room : " + room.number);
+        readFile('ressources/rooms.json').then(
+            function(data){
+                var data = JSON.parse(data);
+                for(var i = 0; i < data.rooms.length; i++){
+                    console.log(data.rooms[i]);
+                    if(data.rooms[i].number == room.number){
+                        console.log("Room " + room.number + " found");
+                        data.rooms[i] = room;
+                        writeFile('ressources/rooms.json', JSON.stringify(data)).then(
+                            function(){
+                                console.log("Emit : roomEdition")
+                                socket.emit("roomEdition", "Room " + room.number + " edition success!");
+                            }, function() {
+                                console.log("Emit : roomEdition")
+                                socket.emit("roomEdition", "Room " + room.number + " edition fail!");
+                            });
+                        break;
+                    }
+                }
+            });
+
+    })
 });
 
 /**
